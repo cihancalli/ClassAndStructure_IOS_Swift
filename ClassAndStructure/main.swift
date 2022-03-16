9fc95c0cd14afc04a5d4931efae510d8e79ee90e
Cihan ÇALLI <cihan.callii@gmail.com>
2022-03-16 21:20:02 +0300

    First Commit

--- ClassAndStructure/main.swift
+++ ClassAndStructure/main.swift
@@ -1,11 +1,45 @@
-//
-//  main.swift
-//  ClassAndStructure
-//
-//  Created by Cihan Çallı on 16.03.2022.
-//
-
 import Foundation
 
-print("Hello, World!")
+
+struct Product {
+    var name:String?
+    var price:Double?
+}
+
+class Car {
+    var color:String?
+    var Capacity:Int?
+}
+
+
+var laptop = Product()
+
+var bmw = Car()
+
+laptop.name = "MacBook"
+laptop.price = 11987.67
+
+bmw.color = "Green"
+bmw.Capacity = 4
+
+print(laptop.name!)
+print(laptop.price!)
+
+print(bmw.color!)
+print(bmw.Capacity!)
+
+if let temp = bmw.color{
+    print(temp)
+}
+
+var tv = Product()
+
+tv.name="Samsung"
+tv.price=8623.32
+
+var limousine = Car()
+
+limousine.color = "Black"
+
+limousine.Capacity = 8
 
